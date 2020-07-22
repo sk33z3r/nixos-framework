b9186ddb154f8176efbc53fdf9cf9a4d1e6405bd
sk33z3r <sk33z3r@protonmail.com>
2020-07-21 20:56:51 -0400

    improve[nextcloud]: change to the official image with auto-configure options

--- docker/nextcloud.nix
+++ docker/nextcloud.nix
@@ -1,12 +1,13 @@
-{ DOMAIN, SQL_PASS }:
+{ DOMAIN, SQL_PASS, USERNAME, PASSWORD }:
 
 let
 
   SERVICE = "nextcloud";
   IP4 = "170";
-  SQLIP = "171";
-  CONFIG_DIR = "/data/${SERVICE}";
-  DATA_DIR = "/data/${SERVICE}/data";
+  SQL_IP = "171";
+  REDIS_IP = "172";
+  DATA_DIR = "/data/${SERVICE}";
+  FILES_DIR = "/data/${SERVICE}/data";
 
 in
 
@@ -16,7 +17,7 @@ in
   system.activationScripts."${SERVICE}" = {
     text = ''
       mkdir -p ${DATA_DIR}
-      mkdir -p ${CONFIG_DIR}
+      mkdir -p ${FILES_DIR}
     '';
     deps = [];
   };
@@ -37,31 +38,52 @@ in
       text = ''
         version: '3.7'
         services:
-          ${SERVICE}:
-            image: linuxserver/nextcloud
-            container_name: ${SERVICE}
+          db:
+            image: mariadb
+            container_name: ${SERVICE}-db
+            command: --transaction-isolation=READ-COMMITTED --binlog-format=ROW
+            restart: always
             volumes:
-              - ${CONFIG_DIR}/config:/config
-              - ${DATA_DIR}:/data
+              - ${DATA_DIR}/db:/var/lib/mysql
             environment:
-              - PUID=1000
-              - PGID=100
-              - TIME_ZONE=America/New_York
+              - MYSQL_ROOT_PASSWORD=${ROOT_PASS}
+              - MYSQL_PASSWORD=${SQL_PASS}
+              - MYSQL_DATABASE=${SERVICE}
+              - MYSQL_USER=${SERVICE}
+            networks:
+              blackrook:
+                ipv4_address: 172.100.0.${SQL_IP}
+          redis:
+            image: redis
+            container_name: ${SERVICE}-redis
+            restart: always
+            volumes:
+              - ${DATA_DIR}/redis:/data
+            networks:
+              blackrook:
+                ipv4_address: 172.100.0.${REDIS_IP}
+          app:
+            image: nextcloud
+            container_name: ${SERVICE}
+            depends_on:
+              - db
+              - redis
+            volumes:
+              - ${DATA_DIR}/app:/var/www/html
+              - ${FILES_DIR}:/data
+            environment:
+              - MYSQL_DATABASE=${SERVICE}
+              - MYSQL_USER=${SERVICE}
+              - MYSQL_PASSWORD=${SQL_PASS}
+              - MYSQL_HOST=172.100.0.${SQL_IP}
+              - NEXTCLOUD_ADMIN_USER=${USERNAME}
+              - NEXTCLOUD_ADMIN_PASSWORD=${PASSWORD}
+              - NEXTCLOUD_DATA_DIR=/data
+              - REDIS_HOST=172.100.0.${REDIS_IP}
             restart: always
             networks:
               blackrook:
                 ipv4_address: 172.100.0.${IP4}
-          db:
-            image: mariadb:latest
-            container_name: ${SERVICE}-mysql
-            environment:
-              - MYSQL_ROOT_PASSWORD=${SQL_PASS}
-              - MYSQL_LOG_CONSOLE=true
-            volumes:
-              - ${CONFIG_DIR}/db:/var/lib/mysql
-            networks:
-              blackrook:
-                ipv4_address: 172.100.0.${SQLIP}
         networks:
           blackrook:
             name: blackrook
