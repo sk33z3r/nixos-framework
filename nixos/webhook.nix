a0d7a5196fe1294cb552eb9df253384c487fefa7
sk33z3r <sk33z3r@protonmail.com>
2020-07-23 12:28:08 -0400

    improve[webhook]: restart command also ensures permissions

--- nixos/webhook.nix
+++ nixos/webhook.nix
@@ -26,7 +26,11 @@
         case $1 in
           start) webhook -hooks /etc/webhooks.json -urlprefix run -port 4004 -verbose -hotreload > /var/www/logs/webhook.log 2>&1 &;;
           stop) pkill webhook;;
-          restart) pkill webhook; webhook -hooks /etc/webhooks.json -urlprefix run -port 4004 -verbose -hotreload > /var/www/logs/webhook.log 2>&1 &;;
+          restart)
+            pkill webhook
+            chown -R root:root /var/www/html
+            webhook -hooks /etc/webhooks.json -urlprefix run -port 4004 -verbose -hotreload > /var/www/logs/webhook.log 2>&1 &
+          ;;
           status) tail -f /var/www/logs/webhook.log;;
           *) echo "Error: Invalid command, expecting [start|stop|restart|status]"; exit 1;;
         esac
