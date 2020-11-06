629a57044a4f74aad27e808042fc0bacc4a83080
sk33z3r <sk33z3r@protonmail.com>
2020-11-06 10:20:31 -0500

    chore[docker]: add console options and help text

--- docker/scripts/helper.nix
+++ docker/scripts/helper.nix
@@ -15,6 +15,20 @@
         #!/usr/bin/env bash
         compose="/etc/${SERVICE}.compose"
         command="docker-compose -f $compose -p ${SERVICE}"
+        usage() {
+        cat <<EOF
+        Commands:
+          up      | Spin up all containers
+          update  | Update all containers
+          down    | Shutdown all containers
+          restart | Restart all containers
+          build   | Build main container
+          logs    | Show logs
+          sh      | Execute /bin/sh inside main container as root
+          bash    | Execute /bin/bash inside main container as root
+          help    | Display this help text
+        EOF
+        }
         case $1 in
           up) $command up -d;;
           update) $command pull; $command up -d --force-recreate;;
@@ -22,7 +36,10 @@
           restart) $command restart;;
           build) $command build ${SERVICE};;
           logs) $command logs -f --tail 100;;
-          *) echo "E: Invalid Command"; exit 1;;
+          sh) $command exec ${SERVICE} sh;;
+          bash) $command exec ${SERVICE} bash;;
+          help) usage;;
+          *) echo "E: Invalid Command"; echo; usage; exit 1;;
         esac
       '';
     };
