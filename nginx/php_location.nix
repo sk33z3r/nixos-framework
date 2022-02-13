b7bdee3c04e3f42d9b8269d464b8f776c13f45ce
sk33z3r <sk33z3r@protonmail.com>
2022-02-13 11:34:45 -0500

    feat[nginx]: add return location blocks for static and PHP sites

---
+++ nginx/php_location.nix
@@ -0,0 +1,47 @@
+{ DOMAIN, SOCKET, NGINXPATH, URI, DEST }:
+
+{
+
+  # Filesystem setup
+  system.activationScripts."${DOMAIN}" = {
+    text = ''
+      mkdir -p /var/www/html/${DOMAIN}
+    '';
+    deps = [];
+  };
+
+  # Static site template
+  services.nginx.appendHttpConfig = "
+    server {
+        listen 443 ssl http2;
+        listen [::]:443 ssl http2;
+
+        server_name ${DOMAIN} www.${DOMAIN};
+        root /var/www/html/${DOMAIN};
+        index index.php index.html;
+
+        # SSL
+        ssl_certificate             /etc/letsencrypt/live/${DOMAIN}/fullchain.pem;
+        ssl_certificate_key         /etc/letsencrypt/live/${DOMAIN}/privkey.pem;
+        ssl_trusted_certificate     /etc/letsencrypt/live/${DOMAIN}/chain.pem;
+
+        # . files
+        location ~ /\\.(?!well-known) {
+            deny all;
+        }
+
+        # rewrite
+        location ~ /${URI} {
+            return 307 https://${DEST};
+        }
+
+        location ~ \\.php$ {
+          fastcgi_pass  unix:${SOCKET};
+          fastcgi_index index.php;
+          include ${NGINXPATH}/conf/fastcgi_params;
+          include ${NGINXPATH}/conf/fastcgi.conf;
+        }
+    }
+  ";
+
+}
