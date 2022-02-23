3d8d9d0d7425c67195cb4fbc01b87ec1dc1b1e71
sk33z3r <sk33z3r@protonmail.com>
2022-02-23 12:27:53 -0500

    chore[php]: add mongo extension

--- nixos/packages/php.nix
+++ nixos/packages/php.nix
@@ -1,6 +1,11 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
+
+  # Extra packages
+  environment.systemPackages = with pkgs; [
+    php74Packages.mongodb
+  ];
 
   services.phpfpm.pools.mypool = {
     user = "phpuser";
@@ -13,6 +18,9 @@
       "pm.max_spare_servers" = 3;
       "pm.max_requests" = 500;
     };
+    phpOptions = ''
+      extension=${pkgs.php74Packages.mongodb}/lib/php/extensions/mongodb.so
+    '';
   };
 
   users.users.phpuser = {
